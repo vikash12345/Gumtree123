41290b1fde7afe832765675ad1e284e5bfc74524
vikash12345 <vikash@backgroundcheck.email>
2016-07-19 09:34:51 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -24,4 +24,27 @@
 // All that matters is that your final data is written to an SQLite database
 // called "data.sqlite" in the current working directory which has at least a table
 // called "data".
+
+print "Hello, coding in the cloud!";           
+$html = scraperWiki::scrape("http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm");           
+print $html . "\n";
+
+require 'scraperwiki/simple_html_dom.php';           
+$dom = new simple_html_dom();
+$dom->load($html);
+foreach($dom->find("div[@align='left'] tr") as $data){
+    $tds = $data->find("td");
+    if(count($tds)==12){
+        $record = array(
+            'country' => $tds[0]->plaintext,
+            'years_in_school' => intval($tds[4]->plaintext)
+        );
+        print json_encode($record) . "\n";
+    }
+}
+
+scraperwiki::save(array('country'), $record);           
+select * from swdata order by years_in_school desc limit 10           
+
+
 ?>
